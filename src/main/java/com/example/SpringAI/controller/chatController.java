046e6f0dfc866e20bbfa10d3d33aba4123ccf30f
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-05-19 13:17:11 +0600

    "Gemini Intregrated update"

--- src/main/java/com/example/SpringAI/controller/chatController.java
+++ src/main/java/com/example/SpringAI/controller/chatController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class chatController {
 
-    private ChatClient chatClient;
+    private final ChatClient chatClient;
 
     public chatController(ChatClient.Builder builder){
         this.chatClient = builder.build();
