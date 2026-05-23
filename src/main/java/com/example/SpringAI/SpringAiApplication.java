65cd1799d239664fdc66fa2deeec24f448090ecb
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-05-23 11:40:50 +0600

    "multiple AI in OpenAi integration"

--- src/main/java/com/example/SpringAI/SpringAiApplication.java
+++ src/main/java/com/example/SpringAI/SpringAiApplication.java
@@ -2,13 +2,17 @@ package com.example.SpringAI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringAiApplication {
 
 	public static void main(String[] args) {
 
-        SpringApplication.run(SpringAiApplication.class, args);
+
+                SpringApplication.run(SpringAiApplication.class, args);
+
+
 	}
 
 }
