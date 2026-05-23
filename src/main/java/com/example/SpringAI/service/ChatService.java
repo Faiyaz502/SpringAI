aaf857f442a3a107b4f4c48258fa0f912c81415e
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-05-23 13:32:10 +0600

    "Ways of Ai Response"

---
+++ src/main/java/com/example/SpringAI/service/ChatService.java
@@ -0,0 +1,14 @@
+package com.example.SpringAI.service;
+
+import com.example.SpringAI.Entity.customResponse;
+
+import java.util.List;
+
+public interface ChatService {
+
+    String chat(String query);
+
+    customResponse chatCustomResponse(String query);
+
+    List<customResponse> chatCustomResponseList(String query);
+}
