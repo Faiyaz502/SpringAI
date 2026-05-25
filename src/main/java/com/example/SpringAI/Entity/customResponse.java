877829371f038c3a0aa6fdeb7f4324cefea94c45
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-05-25 14:45:30 +0600

    "Ways of Ai Response"

---
+++ src/main/java/com/example/SpringAI/Entity/customResponse.java
@@ -0,0 +1,18 @@
+package com.example.SpringAI.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class customResponse {
+
+    private String title ;
+    private String content ;
+    private  String createdYear ;
+
+
+}
