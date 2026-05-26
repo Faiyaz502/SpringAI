549f2883a159a36504980c7b5a857ac6a0de855d
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-05-26 13:38:14 +0600

    "Ways of Ai Response"

--- src/main/java/com/example/SpringAI/controller/chatController.java
+++ src/main/java/com/example/SpringAI/controller/chatController.java
@@ -1,9 +1,14 @@
 package com.example.SpringAI.controller;
 
+import com.example.SpringAI.Entity.customResponse;
+import com.example.SpringAI.service.ChatService;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -13,19 +18,27 @@ public class chatController {
     private final ChatClient groqClient;
     private final ChatClient ollamaClient;
 
+
+
+    // Structured Used
+
+    private ChatService chatService;
+
     public chatController(
             @Qualifier("geminiClient") ChatClient geminiClient,
             @Qualifier("groqClient") ChatClient groqClient,
-            @Qualifier("ollamaClient") ChatClient ollamaClient
+            @Qualifier("ollamaClient") ChatClient ollamaClient,
+            ChatService chatService
     ) {
         this.geminiClient = geminiClient;
         this.groqClient = groqClient;
         this.ollamaClient = ollamaClient;
+        this.chatService = chatService;
     }
 
 
     @GetMapping("/chat/gemini")
-    public ResponseEntity<String> chat(@RequestParam String q) {
+    public ResponseEntity<String> chatWithGemini(@RequestParam String q) {
 
         String response = geminiClient
                 .prompt(q)
@@ -58,4 +71,26 @@ public class chatController {
 
         return ResponseEntity.ok(response);
     }
+
+
+   /// Chat Response
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> chat(@RequestParam String q) {
+
+        return ResponseEntity.ok(chatService.chat(q));
+    }
+
+    @GetMapping("/chat/entity")
+    public ResponseEntity<customResponse> chatResponseEntity(@RequestParam String q) {
+
+        return ResponseEntity.ok(chatService.chatCustomResponse(q));
+    }
+
+    @GetMapping("/chat/entities")
+    public ResponseEntity<List<customResponse>> chatResponseEntities(@RequestParam String q) {
+
+        return ResponseEntity.ok(chatService.chatCustomResponseList(q));
+    }
+
 }
