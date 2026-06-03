e9340b9efe29c7103f8260777dc06e0db41a42d9
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-06-03 10:59:15 +0600

    "Prompt template"

--- src/main/java/com/example/SpringAI/Imp/promptTemplateService.java
+++ src/main/java/com/example/SpringAI/Imp/promptTemplateService.java
@@ -1,8 +1,10 @@
 package com.example.SpringAI.Imp;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -31,7 +33,7 @@ public class promptTemplateService {
 
        String renderMsg = strTemplate.render(Map.of(
                 "techName","Spring",
-                "exampleName","Spring boot"
+                "exampleName","Spring Exception"
         ));
 
        // create prompt
@@ -40,6 +42,34 @@ public class promptTemplateService {
 
 
         return this.chatClient.prompt(prompt).system("As A JAVA EXPERT").call().content();
+    }
+
+    //Add Dynamic System Prompt
+
+
+    public String chatTemplateWithSystemPrompt(String query){
+
+        query = "what is {techName} tell me with examples of {exampleName}";
+
+
+       var SystemTemplate = SystemPromptTemplate.builder().template("You are a expert and assistant of java").build();
+
+       Message systemMessage = SystemTemplate.createMessage();
+
+       var userPromt = PromptTemplate.builder().template(query).build();
+
+
+        Message userMsg = userPromt.createMessage(Map.of(
+                "techName","Spring",
+                "exampleName","Spring Exception"
+        ));
+
+        // create prompt
+
+        Prompt prompt = new Prompt(userMsg,systemMessage);
+
+
+        return this.chatClient.prompt(prompt).call().content();
     }
 
 
