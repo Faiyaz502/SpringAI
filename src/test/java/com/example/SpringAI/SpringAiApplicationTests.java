e6435b76b216675d0f299320382832a41723c0ca
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-06-05 16:13:38 +0600

    "Junit test"

--- src/test/java/com/example/SpringAI/SpringAiApplicationTests.java
+++ src/test/java/com/example/SpringAI/SpringAiApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.SpringAI;
 
+import com.example.SpringAI.Imp.PrompReadingFromFiles;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringAiApplicationTests {
 
+
+    @Autowired
+    private PrompReadingFromFiles prompReadingFromFiles;
+
 	@Test
 	void contextLoads() {
 	}
+    @Test
+    void PromptUsingFile(){
+
+        System.out.println("Running the LLM");
+
+
+        String res = prompReadingFromFiles.UserPromptUsingFile();
+
+
+        System.out.println(res);
+
+
+    }
 
 }
